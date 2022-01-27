735b00df1a358038a2c93dc93235c5afbc6788c7
Nivi106 <93142842+Nivi106@users.noreply.github.com>
2022-01-27 18:09:43 +0100

    modified testcircle and testrectangle

--- TestCircle.py
+++ TestCircle.py
@@ -16,49 +16,54 @@ class TestCircle(unittest.TestCase):
 
 
     def test_getters(self):
-        circle =  Circle((15, 35),10)
-        self.assertEqual((15,35), circle.getCenterPoint())
+        circle =  Circle(Point(15, 35),10)
+        center = (circle.getCenterPoint().getX(),circle.getCenterPoint().getY())
+        self.assertTupleEqual((15,35), center)
+        
         
     def test_setters(self):
-        circle =  Circle((15, 35),10)
+        circle =  Circle(Point(15, 35),10)
         circle.setCenterPoint((15,20))
-        self.assertEqual((15,20) , circle.getCenterPoint())    
+        center = (circle.getCenterPoint())
+        self.assertTupleEqual((15,20),center)
+            
         
     def test_get_radius(self):
-        circle =  Circle((15, 35),10)
+        circle =  Circle(Point(15, 35),10)
         self.assertEqual(10, circle.getRadius())
         
     def test_set_radius(self):
-        circle =  Circle((15, 35),10)
+        circle =  Circle(Point(15, 35),10)
         circle.setRadius(20)
         self.assertEqual(20, circle.getRadius())
         
            
     def test_set_contains(self):
-        x=Point(5,5)
-        y =  Circle((15, 35),10)
-        self.assertEqual(y.contains(x),'True')        
+        x=Point(20,36)
+        y =  Circle(Point(15, 35),10)
+        self.assertEqual(y.contains(x),True)        
         
     def test_area(self):
-        circle =  Circle((15, 35),10)
+        circle =  Circle(Point(15, 35),10)
         self.assertEqual(314.2,circle.area())         
         
     def test_perimeter(self):
-        circle =  Circle((15, 35),10)
+        circle =  Circle(Point(15, 35),10)
         self.assertEqual(62.839999999999996,circle.perimeter()) 
                 
             
     def test_to_string(self):
-        circle = Circle((15, 35),10)
-        self.assertEqual('Circle: center: (15, 35),radius: 10',circle.tostring())
+        circle = Circle((15,35),10)
+        x = circle.tostring()
+        self.assertEqual('Circle: center: (15, 35),radius: 10',x)
 
     def test_getters_shape(self):
-        circle =  Circle((15, 35),10)
+        circle =  Circle(Point(15, 35),10)
         circle.setters(10, 'red', 'blue')
         self.assertEqual((10, 'red', 'blue'),circle.getters())  
         
     def test_setters_shape(self):
-        circle =  Circle((15, 35),10)
+        circle =  Circle(Point(15, 35),10)
         circle.setters(10, 'red', 'blue')
         self.assertEqual((10,'red','blue'), circle.getters())  
         
--- TestRectangle.py
+++ TestRectangle.py
@@ -15,51 +15,54 @@ class TestRectangle(unittest.TestCase):
 
 
     def test_getters(self):
-        Rect =  Rectangle((15, 35),10,20)
-        self.assertEqual((15,35), Rect.getting())
+        Rect =  Rectangle(Point(15, 35),10,20)
+        center = (Rect.topleftpoint.getX(),Rect.topleftpoint.getY())
+        self.assertEqual((15,35), center)
+        
         
     def test_setters(self):
-        rect =  Rectangle((15, 35),10,20)
+        rect =  Rectangle(Point(15, 35),10,20)
         rect.setting((15,20))
         self.assertEqual((15,20) , rect.getting())
 
 
     def test_get_width(self):
-        Rect =  Rectangle((15, 35),10,20)
+        Rect =  Rectangle(Point(15, 35),10,20)
         self.assertEqual(10, Rect.getwidth())
         
     def test_set_width(self):
-        rect =  Rectangle((15, 35),10,20)
+        rect =  Rectangle(Point(15, 35),10,20)
         rect.setwidth((15))
         self.assertEqual(15, rect.getwidth())  
         
     def test_get_height(self):
-        Rect =  Rectangle((15, 35),10,20)
+        Rect =  Rectangle(Point(15, 35),10,20)
         self.assertEqual(20, Rect.getheight())
         
     def test_set_height(self):
         
-        rect =  Rectangle((15, 35),10,20)
+        rect =  Rectangle(Point(15, 35),10,20)
         rect.setheight((15))
         self.assertEqual(15, rect.getheight())    
         
     def test_set_contains(self):
-        x=Point(5,5)
-        y=Rectangle((15, 35),10,20)
-        self.assertEqual(y.contains(x),'True')
+        x=Point(16,36)
+        y=Rectangle(Point(15, 35),10,20)
+        self.assertEqual(y.contains(x),True)
                   
         
     def test_area(self):
-        Rect =  Rectangle((15, 35),10,20)
+        Rect =  Rectangle(Point(15, 35),10,20)
         self.assertEqual(200,Rect.area())         
         
     def test_perimeter(self):
-        Rect =  Rectangle((15, 35),10,20)
+        Rect =  Rectangle(Point(15, 35),10,20)
         self.assertEqual(60,Rect.perimeter()) 
 
     def test_set_centroid(self):
-        Rect =  Rectangle((15, 35),10,20)
-        self.assertEqual((20.0, 45.0),Rect.centroid())         
+        Rect =  Rectangle(Point(15, 35),10,20)
+        x = (Rect.centroid().getX(), Rect.centroid().getY())
+        self.assertTupleEqual((20.0,45.0), x) 
         
                
     def test_to_string(self):
